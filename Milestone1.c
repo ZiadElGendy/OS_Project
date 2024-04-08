2bf3e82da2dfedf2175bf92537821b003eba965a
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2024-04-08 23:47:36 +0200

    adjust code

--- Milestone1.c
+++ Milestone1.c
@@ -2,44 +2,36 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-// Function prototypes for thread functions
-void *thread_function(void *arg);
+void thread_function(void *arg)
+{
+    int thread_id = (int)arg;
 
-int main() {
-    pthread_t threads[4]; // Array to hold thread IDs
+    printf("Thread %d started\n", thread_id);
+    printf("Thread %d executing statement 1\n", thread_id);
+    printf("Thread %d executing statement 2\n", thread_id);
+    printf("Thread %d finished\n", thread_id);
+
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t threads[4];
     int i;
 
-    // Create threads
-    for (i = 0; i < 4; i++) {
-        if (pthread_create(&threads[i], NULL, thread_function, (void *)(intptr_t)i) != 0) {
-            perror("pthread_create");
-            exit(EXIT_FAILURE);
-        }
+    for (i = 0; i < 4; i++)
+    {
+        pthread_create(&threads[i], NULL, thread_function, (void*)(&threads[i]));
     }
 
-    // Join threads
-    for (i = 0; i < 4; i++) {
-        if (pthread_join(threads[i], NULL) != 0) {
+    for (i = 0; i < 4; i++)
+    {
+        if (pthread_join(threads[i], NULL) != 0)
+        {
             perror("pthread_join");
             exit(EXIT_FAILURE);
         }
     }
 
     return 0;
-}
-
-// Thread function
-void *thread_function(void *arg) {
-    int thread_id = (int)(intptr_t)arg;
-
-    // Print statements
-    printf("Thread %d started\n", thread_id);
-    // Additional print statements...
-    printf("Thread %d executing statement 1\n", thread_id);
-    // Additional print statements...
-    printf("Thread %d executing statement 2\n", thread_id);
-    // Additional print statements...
-    printf("Thread %d finished\n", thread_id);
-
-    pthread_exit(NULL);
 }
