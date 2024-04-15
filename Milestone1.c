0bb6fadaef3d8006217567598a4769bfe2a2f05d
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2024-04-15 22:12:32 +0200

    add prints for ewxecution time

--- Milestone1.c
+++ Milestone1.c
@@ -1,100 +1,97 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sched.h>
+#include <unistd.h>
 
-void thread_short_function(void *arg)
-{
-    int thread_id = *((int *)arg);
-
-    printf("Thread %d started\n", thread_id);
-    for (int i = 0; i < 10; i++)
+void busyWork() {
+    for (int j = 0; j < 99999999; j++)
     {
-        if (i % 2 == 0)
-            printf("Thread %d is running\n", thread_id);
-            sleep(1);
+        int i = j + 1;
+        i = i - 1;
+        i = i * 2;
+        i = i / 2;
+        i = i + 1;
+        i = i - 1;
+        i = i * 2;
+        i = i % 2;
+        int result = j * j;
     }
-    printf("Thread %d finished\n", thread_id);
-
-    pthread_exit(NULL);
 }
 
-void thread_medium_function(void *arg)
+void *func(void *arg)
 {
     int thread_id = *((int *)arg);
 
-    printf("Thread %d started\n", thread_id);
-    for (int i = 0; i < 100; i++)
-    {
-        if (i % 25 == 0)
-            printf("Thread %d is running\n", thread_id);
-    }
-    printf("Thread %d finished\n", thread_id);
-
-    pthread_exit(NULL);
-}
-
-void thread_long_function(void *arg)
-{
-    int thread_id = *((int *)arg);
+    struct timespec start_time, end_time;
 
     printf("Thread %d started\n", thread_id);
-    for (int i = 0; i < 1000; i++)
+
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+    busyWork();
+
+    for (int i = 0; i < 5; i++)
     {
-        if (i % 250 == 0)
-            printf("Thread %d is running\n", thread_id);
+        printf("Thread %d is running %d\n", thread_id, i + 1);
+        busyWork();
     }
+
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+
+    double execution_time = (end_time.tv_sec - start_time.tv_sec) +
+                            (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
+
     printf("Thread %d finished\n", thread_id);
+    printf("Thread %d start time: %ld.%09ld\n", thread_id, start_time.tv_sec, start_time.tv_nsec);
+    printf("Thread %d end time: %ld.%09ld\n", thread_id, end_time.tv_sec, end_time.tv_nsec);
+    printf("Thread %d execution time: %.9f seconds\n", thread_id, execution_time);
 
     pthread_exit(NULL);
 }
 
 int main()
 {
-    pthread_t threads[4];
-    int i, j;
+    pthread_t ptid1;
+    pthread_t ptid2;
+    pthread_t ptid3;
+    pthread_t ptid4;
+    
+    int thread_id1 = 1;
+    int thread_id2 = 2;
+    int thread_id3 = 3;
+    int thread_id4 = 4;
+
     pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    // Set scheduling policy to SCHED_FIFO
     struct sched_param param;
-    int policies[3] = {SCHED_FIFO, SCHED_RR, SCHED_OTHER};
+    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    //pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM); seems unnecessary, doesnt affect results
+    param.sched_priority = sched_get_priority_max(SCHED_RR);
+    pthread_attr_setschedpolicy(&attr, SCHED_RR);
+    pthread_attr_setschedparam(&attr, &param);
 
-    for (j = 0; j < 3; j++)
-    {
-        pthread_attr_init(&attr);
-        pthread_attr_setschedpolicy(&attr, policies[j]);
-        param.sched_priority = sched_get_priority_max(policies[j]);
-        pthread_attr_setschedparam(&attr, &param);
+    // Set CPU affinity to ensure all threads run on the same CPU
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(0, &cpuset); // Set to CPU 0, you can change it according to your system
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuset);
 
-        for (i = 0; i < 4; i++)
-        {
-            int *thread_id = malloc(sizeof(int));
-            *thread_id = i;
-            switch (i)
-            {
-            case 0:
-                pthread_create(&threads[i], &attr, thread_short_function, (void *)thread_id);
-                break;
-            case 1:
-                pthread_create(&threads[i], &attr, thread_medium_function, (void *)thread_id);
-                break;
-            case 2:
-                pthread_create(&threads[i], &attr, thread_short_function, (void *)thread_id);
-                break;
-            case 3:
-                pthread_create(&threads[i], &attr, thread_long_function, (void *)thread_id);
-                break;
-            }
-        }
-
-        for (i = 0; i < 4; i++)
-        {
-            if (pthread_join(threads[i], NULL) != 0)
-            {
-                perror("pthread_join");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        pthread_attr_destroy(&attr);
+    // Check if the effective user id is root
+    if (geteuid() != 0) {
+        printf("Need root privileges to set real-time scheduling policy.\n");
+        exit(EXIT_FAILURE);
     }
 
-    return 0;
+    pthread_create(&ptid1, &attr, &func, (void *)&thread_id1);
+    pthread_create(&ptid2, &attr, &func, (void *)&thread_id2);
+    pthread_create(&ptid3, &attr, &func, (void *)&thread_id3);
+    pthread_create(&ptid4, &attr, &func, (void *)&thread_id4); 
+
+    pthread_attr_destroy(&attr);
+
+    pthread_exit(NULL);
 }
