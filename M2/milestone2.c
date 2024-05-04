305140acb8a6e65ea9c934be586e31c1b4487c6d
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2024-05-04 14:11:09 +0300

    add queue implementation from CA

--- M2/milestone2.c
+++ M2/milestone2.c
@@ -1,55 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
+#pragma region int queue implementation
+struct Queue
 {
-    int val;
-    struct node *next;
-}node_t;
+    int front, rear, size;
+    unsigned capacity;
+    int *array;
+};
 
-void enqueue(node_t **head, int val)
+// function to create a queue of given capacity. It initializes size of queue as 0
+struct Queue *createQueue(unsigned capacity)
 {
-    node_t *new_node = malloc(sizeof(node_t));
-    if (!new_node)
+    struct Queue *queue = (struct Queue *)malloc(
+        sizeof(struct Queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+
+    // This is important, see the enqueue
+    queue->rear = capacity - 1;
+    queue->array = (int *)malloc(
+        queue->capacity * sizeof(int));
+    return queue;
+}
+
+// Queue is full when size becomes equal to the capacity
+int isFull(struct Queue *queue)
+{
+    return (queue->size == queue->capacity);
+}
+
+// Queue is empty when size is 0
+int isEmpty(struct Queue *queue)
+{
+    return (queue->size == 0);
+}
+
+// Function to add an item to the queue. It changes rear and size
+void enqueue(struct Queue *queue, int item)
+{
+    if (isFull(queue))
         return;
-
-    new_node->val = val;
-    new_node->next = *head;
-
-    *head = new_node;
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size = queue->size + 1;
+    // printf("%d enqueued to queue\n", item);
 }
 
-int dequeue(node_t **head)
+// Function to remove an item from queue. It changes front and size
+int dequeue(struct Queue *queue)
 {
-    node_t *current, *prev = NULL;
-    int retval = -1;
-
-    if (*head == NULL)
-        return -1;
-
-    current = *head;
-    while (current->next != NULL)
-    {
-        prev = current;
-        current = current->next;
-    }
-
-    retval = current->val;
-    free(current);
-
-    if (prev)
-        prev->next = NULL;
-    else
-        *head = NULL;
-
-    return retval;
+    if (isEmpty(queue))
+        return INT_MIN;
+    int item = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size = queue->size - 1;
+    return item;
 }
 
-node_t priority1Queue = {0, NULL};
-node_t priority2Queue = {0, NULL};
-node_t priority3Queue = {0, NULL};
-node_t priority4Queue = {0, NULL};
-node_t blockedQueue = {0, NULL};
+// Function to get front of queue
+int front(struct Queue *queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->front];
+}
+#pragma endregion
+
+struct Queue priority1Queue;
+struct Queue priority2Queue;
+struct Queue priority3Queue;
+struct Queue priority4Queue;
+struct Queue blockedQueue;
 
 struct PCB {
     int pid;
@@ -75,6 +97,7 @@ struct memory {
 };
 
 int main(){
+    
     while (readTxtFile() != null){
         createNewProcess();
         while (readline() != null)
