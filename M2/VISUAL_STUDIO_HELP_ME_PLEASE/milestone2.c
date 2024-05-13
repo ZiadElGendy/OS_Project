bb0ae8e6746b00ab3fde47b08925baf16a0b8dfe
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2024-05-14 00:43:02 +0300

    load programs into memory properly

--- M2/VISUAL_STUDIO_HELP_ME_PLEASE/milestone2.c
+++ M2/VISUAL_STUDIO_HELP_ME_PLEASE/milestone2.c
@@ -125,6 +125,8 @@ char** readFile(int programNum)
     // Read lines until the end of the file
     while (fgets(line, sizeof(line), file) != NULL)
     {
+		line[strcspn(line, "\n")] = 0; //Remove the newline character from the line
+
 		char** tmp = realloc(lines, (i + 1) * sizeof(*lines));
 		if (tmp == NULL)
 		{
@@ -143,6 +145,8 @@ char** readFile(int programNum)
 			free(lines);
 			return NULL;
 		}
+
+
         i++;
     }
 	lines[i] = NULL;
@@ -153,7 +157,7 @@ char** readFile(int programNum)
     return lines;
 }
 
-void loadProgramIntoMemory(int processId, char** lines, int numLines)
+void loadProgramIntoMemory(int processId, char** lines)
 {
     //Find the first available memory block
     int lowerMemoryBound = 0;
@@ -165,48 +169,54 @@ void loadProgramIntoMemory(int processId, char** lines, int numLines)
             break;
         }
     }
+
+    int numLines = 0;
+    while (lines[numLines] != NULL) {
+        numLines++;
+    }
+
     int upperMemoryBound = lowerMemoryBound + 9 + numLines; //5 for PCB, 3 for process variables, 1 for quantum, and the number of lines in the program
 
     //Load PCB into memory
-    strcpy_s(memory.words[lowerMemoryBound].name, sizeof(memory.words[lowerMemoryBound].name), "pid");
-    sprintf_s(memory.words[lowerMemoryBound].data, sizeof(memory.words[lowerMemoryBound].data), "%d", processId);
+    strcpy(memory.words[lowerMemoryBound].name, "pid");
+    sprintf(memory.words[lowerMemoryBound].data, "%d", processId);
 
-    strcpy_s(memory.words[lowerMemoryBound + 1].name, sizeof(memory.words[lowerMemoryBound].name), "processState");
-    strcpy_s(memory.words[lowerMemoryBound + 1].data, sizeof(memory.words[lowerMemoryBound].data), "ready");
+    strcpy(memory.words[lowerMemoryBound + 1].name, "processState");
+    strcpy(memory.words[lowerMemoryBound + 1].data, "ready");
                                                        
-    strcpy_s(memory.words[lowerMemoryBound + 2].name, sizeof(memory.words[lowerMemoryBound].name), "currentPriority");
-    strcpy_s(memory.words[lowerMemoryBound + 2].data, sizeof(memory.words[lowerMemoryBound].data), "1");
+    strcpy(memory.words[lowerMemoryBound + 2].name, "currentPriority");
+    strcpy(memory.words[lowerMemoryBound + 2].data, "1");
 
-    strcpy_s(memory.words[lowerMemoryBound + 3].name, sizeof(memory.words[lowerMemoryBound].name), "programCounter");
-    strcpy_s(memory.words[lowerMemoryBound + 3].data, sizeof(memory.words[lowerMemoryBound].data), "0");
+    strcpy(memory.words[lowerMemoryBound + 3].name, "programCounter");
+    strcpy(memory.words[lowerMemoryBound + 3].data, "0");
 
-    strcpy_s(memory.words[lowerMemoryBound + 4].name, sizeof(memory.words[lowerMemoryBound].name), "lowerMemoryBound");
-    sprintf_s(memory.words[lowerMemoryBound + 4].data,sizeof(memory.words[lowerMemoryBound].data),  "%d", lowerMemoryBound);
+    strcpy(memory.words[lowerMemoryBound + 4].name, "lowerMemoryBound");
+    sprintf(memory.words[lowerMemoryBound + 4].data,  "%d", lowerMemoryBound);
 
-    strcpy_s(memory.words[lowerMemoryBound + 5].name, sizeof(memory.words[lowerMemoryBound].name), "higherMemoryBound");
-    sprintf_s(memory.words[lowerMemoryBound + 5].data, sizeof(memory.words[lowerMemoryBound].data),  "%d", upperMemoryBound);
+    strcpy(memory.words[lowerMemoryBound + 5].name, "higherMemoryBound");
+    sprintf(memory.words[lowerMemoryBound + 5].data,  "%d", upperMemoryBound);
 
 
     //Load process variables into memory
-    strcpy_s(memory.words[lowerMemoryBound + 6].name,sizeof(memory.words[lowerMemoryBound].name),  "x");
-    strcpy_s(memory.words[lowerMemoryBound + 6].data,sizeof(memory.words[lowerMemoryBound].data), "0");
+    strcpy(memory.words[lowerMemoryBound + 6].name,  "x");
+    strcpy(memory.words[lowerMemoryBound + 6].data, "0");
                                                                                                 
-    strcpy_s(memory.words[lowerMemoryBound + 7].name,sizeof(memory.words[lowerMemoryBound].name), "y");
-    strcpy_s(memory.words[lowerMemoryBound + 7].data,sizeof(memory.words[lowerMemoryBound].data), "0");
+    strcpy(memory.words[lowerMemoryBound + 7].name, "y");
+    strcpy(memory.words[lowerMemoryBound + 7].data, "0");
                                                                                                 
-    strcpy_s(memory.words[lowerMemoryBound + 8].name,sizeof(memory.words[lowerMemoryBound].name), "z");
-    strcpy_s(memory.words[lowerMemoryBound + 8].data,sizeof(memory.words[lowerMemoryBound].data), "0");
+    strcpy(memory.words[lowerMemoryBound + 8].name, "z");
+    strcpy(memory.words[lowerMemoryBound + 8].data, "0");
 
     //Load program into memory
     for (int i = 0; i < numLines; i++)
     {
-        sprintf_s(memory.words[lowerMemoryBound + 9 + i].name, sizeof(memory.words[lowerMemoryBound].name), "line %d", i);
-        strcpy_s(memory.words[lowerMemoryBound + 9 + i].data, sizeof(memory.words[lowerMemoryBound].data), lines[i]);
+        sprintf(memory.words[lowerMemoryBound + 9 + i].name, "line %d", i);
+        strcpy(memory.words[lowerMemoryBound + 9 + i].data, lines[i]);
     }
 
     //Load quantum into memory
-    strcpy_s(memory.words[upperMemoryBound].name, sizeof(memory.words[lowerMemoryBound].name), "quantum");
-	strcpy_s(memory.words[upperMemoryBound].data, sizeof(memory.words[lowerMemoryBound].data), "0");
+    strcpy(memory.words[upperMemoryBound].name, "quantum");
+	strcpy(memory.words[upperMemoryBound].data, "0");
 }
 
 void printMemoryContents()
@@ -225,12 +235,11 @@ int main() {
     priority4Queue = *createQueue(QUEUE_CAPACITY);
     blockedQueue = *createQueue(QUEUE_CAPACITY);
 
-	char** lines = readFile(1);
-
-    int lineCount = 0;
-    while (lines[lineCount] != NULL) {
-        lineCount++;
-    }
-    printf("Number of lines: %d\n", lineCount);
-    Sleep(5000);
+	for (int i = 1; i <= 3; i++)
+    {
+        char** lines = readFile(i);
+        loadProgramIntoMemory(i, lines);
+	}
+	printMemoryContents();
+    Sleep(1000000);
 }
